8367aa0cbe360c98aa6d37f19904a308739f75ef
Akila Kumanjana <akilakumanjanag@gmail.com>
2025-04-23 18:15:22 +0530

    All

--- obj/Debug/net9.0/AskHire-Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/AskHire-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AskHire-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1882c418c933475db7004f2f25edf2566ff399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3d2e5c89a9a043e1fce1fbe6597d668901e8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AskHire-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AskHire-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
