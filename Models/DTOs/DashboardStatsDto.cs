96ba6c26a54d63c3cbf3557e3669ecb1493a724f
senuri balasooriya <senuribalasuriya@gmail.com>
2025-04-29 16:50:23 +0530

    new dashbord

---
+++ Models/DTOs/DashboardStatsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AskHire_Backend.Models.DTOs
+{
+    public class DashboardStatsDto
+    {
+        public int TotalUsers { get; set; }
+        public int TotalCandidates { get; set; }
+        public int TotalManagers { get; set; }
+        public int TotalJobs { get; set; }
+    }
+}
