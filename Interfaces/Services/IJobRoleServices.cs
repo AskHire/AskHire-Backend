d49933065cd0b556cc4284da4370ef5d76345568
eshancool123 <eshansenadhi5@gmail.com>
2025-05-20 14:06:03 +0530

    remove comment

--- Interfaces/Services/IJobRoleServices.cs
+++ Interfaces/Services/IJobRoleServices.cs
@@ -12,6 +12,5 @@ namespace AskHire_Backend.Interfaces.Services
         Task<IEnumerable<JobRole>> GetAllJobRolesAsync();
         Task<bool> DeleteJobRoleAsync(Guid id);
         Task<JobRole?> UpdateJobRoleAsync(JobRole jobRole);  // Nullable return type
-        // Task<int> GetTotalJobsAsync();
     }
 }
