2e30e4f921acdb9edaebf94f41abe03191cdce83
Dimashi2002 <dimashiwickramage2002@gmail.com>
2025-07-18 16:38:32 +0530

    Updated Dashboard Status

--- Interfaces/Repositories/ManagerRepositories/IManagerInterviewRepository.cs
+++ Interfaces/Repositories/ManagerRepositories/IManagerInterviewRepository.cs
@@ -1,21 +1,20 @@
-using AskHire_Backend.Data.Entities;
-using AskHire_Backend.Models.DTOs.CandidateDTOs;
 using AskHire_Backend.Models.Entities;
+using AskHire_Backend.Models.DTOs.CandidateDTOs;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace AskHire_Backend.Repositories.Interfaces
+namespace AskHire_Backend.Interfaces.Repositories.ManagerRepositories
 {
     public interface IManagerInterviewRepository
     {
+        // ... other method signatures ...
         Task<Application> GetApplicationWithUserAsync(Guid applicationId);
         Task<Interview> CreateInterviewAsync(Interview interview);
         Task<Interview> UpdateInterviewAsync(Interview interview);
         Task<Interview> GetInterviewByApplicationIdAsync(Guid applicationId);
         Task<List<Interview>> GetAllInterviewsAsync();
         Task<List<UserInterviewDetailsDto>> GetInterviewsByUserIdAsync(Guid userId);
-     
-
+        Task UpdateApplicationAsync(Application application);
     }
-}
+} 
